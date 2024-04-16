7a2c11a071649e81159afff6ac348249aa01700c
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-04-16 17:11:24 -0500

    initial auth files

---
+++ src/main/java/org/rg25/auth/CognitoTokenHeader.java
@@ -0,0 +1,20 @@
+package org.rg25.auth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class CognitoTokenHeader{
+
+	@JsonProperty("kid")
+	private String kid;
+
+	@JsonProperty("alg")
+	private String alg;
+
+	public String getKid(){
+		return kid;
+	}
+
+	public String getAlg(){
+		return alg;
+	}
+}
