daae34c0283a41f1b9b4157a7c3cbe825e6b1a4a
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-04-17 02:01:22 -0500

    Improved user entity

--- src/main/java/org/rg25/entity/User.java
+++ src/main/java/org/rg25/entity/User.java
@@ -4,6 +4,7 @@ package org.rg25.entity;
 import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @Entity(name = "User")
 @Table(name = "user")
@@ -58,5 +59,27 @@ public class User {
 
     public void setTodos(List<Todo> todos) {
         this.todos = todos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return user_id == user.user_id && Objects.equals(username, user.username) && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user_id, username, name);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "user_id=" + user_id +
+                ", username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
