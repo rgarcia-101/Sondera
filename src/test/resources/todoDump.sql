8db8a8776275ae4e3acd16d760dd73f5379a1e4c
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-04-17 02:01:53 -0500

    fixed wrong input

--- src/test/resources/todoDump.sql
+++ src/test/resources/todoDump.sql
@@ -1,2 +1,2 @@
 INSERT INTO todo(title, content, created, completed, due_date, user_id)
-    VALUES ('code','do','123',0,'2020-11-11 12:43:15',1);
+    VALUES ('code','do','2019-10-10 12:34:56',0,'2020-11-11 12:43:15',1);
