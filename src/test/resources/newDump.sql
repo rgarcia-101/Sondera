ea41d47b887760dbc42a8b2fe0558624e6146789
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-04-17 02:00:08 -0500

    updated dump

--- src/test/resources/newDump.sql
+++ src/test/resources/newDump.sql
@@ -1,12 +1,15 @@
 
 DROP TABLE IF EXISTS `todo`;
+DROP TABLE IF EXISTS `note`;
+DROP TABLE IF EXISTS `bookmark`;
+DROP TABLE IF EXISTS `date`;
 DROP TABLE IF EXISTS `user`;
 
 create table user
 (
     user_id       int auto_increment
         primary key,
-    password varchar(50) not null,
+    username varchar(50) not null,
     name     varchar(50) not null
 );
 
@@ -19,8 +22,8 @@ create table todo
     id        int auto_increment
         primary key,
     title     varchar(20)          not null,
-    content   text                 not null,
-    created   varchar(10)          not null,
+    content   varchar (500)        not null,
+    created   datetime             not null,
     completed tinyint(1) default 0 not null,
     due_date  datetime             not null,
     user_id   int                  not null,
@@ -28,5 +31,20 @@ create table todo
         foreign key (user_id) references user (user_id)
 );
 
-LOCK TABLES `todo`WRITE;
+LOCK TABLES `todo` WRITE;
+UNLOCK TABLES;
+
+CREATE TABLE `note`
+(
+    id        int auto_increment
+        primary key,
+    title     varchar(20)          not null,
+    content   text                 not null,
+    created   datetime             not null,
+    user_id   int                  not null,
+    constraint note_user_id_fk
+        foreign key (user_id) references user (user_id)
+);
+
+LOCK TABLES note WRITE;
 UNLOCK TABLES;
