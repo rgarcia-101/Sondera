89c3d4ae98ab9e1e5339c1c34a764f7fa3030f94
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-04-17 02:00:48 -0500

    made sql file for notes

---
+++ src/test/resources/noteDump.sql
@@ -0,0 +1,4 @@
+INSERT INTO note(title, content, created, user_id)
+VALUES ('code','do','2019-10-10 12:34:56',1);
+INSERT INTO note(title, content, created, user_id)
+VALUES ('new','note','2018-10-10 12:34:56',1);
