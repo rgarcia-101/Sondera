0e2e635e3e701facab99509a0d900539a0dd81c6
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-04-21 03:02:50 -0500

    Updated user to match updated schema

--- src/main/java/org/rg25/entity/User.java
+++ src/main/java/org/rg25/entity/User.java
@@ -16,16 +16,28 @@ public class User {
     private int user_id;
     @Column(name = "username")
     private String username;
-    @Column(name = "name")
-    private String name;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column
+    private String email;
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Todo> todos;
 
     public User() {
-
     }
 
-    public User(int id, String name, String username) {
+    public User(User newUser) {
+        username = newUser.getUsername();
+        firstName = newUser.getFirstName();
+        lastName = newUser.getLastName();
+        email = newUser.getEmail();
+        user_id = newUser.getId();
+        todos = newUser.getTodos();
+    }
+
+    public User(int id, String firstName, String lastName, String username, String email) {
 
     }
 
@@ -45,13 +57,30 @@ public class User {
         this.username = username;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 
     public List<Todo> getTodos() {
         return todos;
@@ -62,24 +91,46 @@ public class User {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return user_id == user.user_id && Objects.equals(username, user.username) && Objects.equals(name, user.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(user_id, username, name);
-    }
-
-    @Override
     public String toString() {
         return "User{" +
                 "user_id=" + user_id +
                 ", username='" + username + '\'' +
-                ", name='" + name + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
                 '}';
+    }
+
+    /**
+     * Compares two objects
+     * does not compare id, for simplified convenience
+     * @param o object to compare
+     * @return if object equals this
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(email, user.email);
+    }
+
+    /**
+     * Compares two objects
+     * does compare id, for strict testing cases
+     * @param o object to compare
+     * @return if object equals this
+     */
+    public boolean equalsWithId(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return user_id == user.user_id && Objects.equals(username, user.username) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(email, user.email);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user_id, username, firstName, lastName, email);
     }
 }
