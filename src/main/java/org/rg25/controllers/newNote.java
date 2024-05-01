15dd106ff4e5a6da6674cd43751e9f38183b919a
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-05-01 00:17:24 -0500

    Controller to handle making new notes

---
+++ src/main/java/org/rg25/controllers/newNote.java
@@ -0,0 +1,47 @@
+package org.rg25.controllers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.rg25.entity.Note;
+import org.rg25.entity.User;
+import org.rg25.persistance.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet (
+        urlPatterns = ("/newNote")
+)
+public class newNote extends HttpServlet {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    private GenericDao<Note> noteDao = new GenericDao<>(Note.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("user");
+
+        // No user, default to error page
+        if (session.getAttribute("user") == null) {
+            RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/error.jsp");
+            dispatch.forward(req, resp);
+            return;
+        }
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        Note note = new Note(user, "New Note", "", format.format(date));
+
+        int id = noteDao.insert(note);
+
+        resp.sendRedirect("noteEditor?id=" + id);
+    }
+}
