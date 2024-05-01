d62f6ccad9857c6db6d7553e02a337715c02fd4a
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-05-01 03:19:09 -0500

    Adding dates functionality

---
+++ src/main/java/org/rg25/controllers/DatesHomeController.java
@@ -0,0 +1,38 @@
+package org.rg25.controllers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.rg25.entity.Date;
+import org.rg25.entity.Note;
+import org.rg25.entity.User;
+import org.rg25.persistance.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(
+        urlPatterns = ("dates")
+)
+public class DatesHomeController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("user");
+
+        // No user, default to error page
+        if (session.getAttribute("user") == null) {
+            RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/error.jsp");
+            dispatch.forward(req, resp);
+            return;
+        }
+
+        List<Date> dates;
+    }
+    }
--- src/main/java/org/rg25/entity/Date.java
+++ src/main/java/org/rg25/entity/Date.java
@@ -1,4 +1,76 @@
 package org.rg25.entity;
 
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity(name = "Date")
+@Table(name = "date")
 public class Date {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+
+    private String date;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Date date1 = (Date) o;
+        return id == date1.id && Objects.equals(title, date1.title) && Objects.equals(date, date1.date) && Objects.equals(content, date1.content) && Objects.equals(user, date1.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, date, content, user);
+    }
 }
