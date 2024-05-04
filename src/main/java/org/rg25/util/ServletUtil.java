8537a7cf7744e4cbb566098847601772f140dd77
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-05-04 03:05:18 -0500

    Util servlet update

--- src/main/java/org/rg25/util/ServletUtil.java
+++ src/main/java/org/rg25/util/ServletUtil.java
@@ -1,10 +1,12 @@
 package org.rg25.util;
 
+import com.google.gson.JsonObject;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
+import java.io.PrintWriter;
 
 public class ServletUtil {
     private final Logger logger = LogManager.getLogger(this.getClass());
@@ -21,6 +23,30 @@ public class ServletUtil {
             logger.error("Could not read! " + e);
         }
 
+
         return buffer;
+    }
+
+    /**
+     * Processes the JSON for instances where no changes to the database are needed
+     * @param writer writer to write JSON to
+     */
+    public void approveEquivalentJson(PrintWriter writer) {
+        logger.info("Text is identical, not saving...");
+        JsonObject jsonResponse = new JsonObject();
+        jsonResponse.addProperty("responseCode", "0");
+        writer.print(jsonResponse.toString());
+        writer.flush();
+    }
+
+    /**
+     * Processes the JSON for instances where the request was denied
+     * @param writer writer to write JSON to
+     */
+    public void denyJson(PrintWriter writer) {
+        JsonObject jsonResponse = new JsonObject();
+        jsonResponse.addProperty("responseCode", "1");
+        writer.print(jsonResponse.toString());
+        writer.flush();
     }
 }
