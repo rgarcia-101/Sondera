7e35382300e6aa9d76e77977b92f7833c5239e7a
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-05-06 19:57:02 -0500

    todo Javascript

--- src/main/webapp/todo.js
+++ src/main/webapp/todo.js
@@ -2,15 +2,17 @@ let ctrl = false;
 let textArea;
 let saveText;
 let disappearCount;
-let noteTitle
-let noteId;
+let todoTitle
+let todoId;
+let dueDate;
 
 // Ctrl + s feature
 window.addEventListener('load', function() {
     textArea = document.querySelector('#textInput');
     saveText = document.querySelector('#saveText');
-    noteId = new URLSearchParams(window.location.search).get('id');
-    noteTitle = document.querySelector('#noteTitle');
+    todoId = new URLSearchParams(window.location.search).get('id');
+    todoTitle = document.querySelector('#noteTitle');
+    dueDate = document.querySelector('#dueDate')
     disappearCount = 0;
     textArea.addEventListener('keydown',function (e) {
         if (e.keyCode === 17) {
@@ -42,20 +44,21 @@ window.addEventListener('load', function() {
     });
 
 })
-const note = async () => {
-    // TODO enforce limit on title (25 as of now)
+const todo = async () => {
 
+    console.log(`${dueDate.value}`);
     saveText.innerHTML = "";
 
     let bodyData = JSON.stringify({
         'textContent': `${textArea.value}`,
-        'id': `${noteId}`,
-        'noteTitle': `${noteTitle.value}`
+        'id': `${todoId}`,
+        'todoTitle': `${todoTitle.value}`,
+        'dueDate': `${dueDate.value}`
     });
 
     let header = {'Content-Type': 'application/json'}
 
-    const reply =  fetch('noteEditor', {
+    const reply =  fetch('todoEditor', {
         method: 'PUT',
         body: bodyData,
         headers: header
