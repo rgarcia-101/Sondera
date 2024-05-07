9ba63ed1242f94f13c479aec158af9bfcec41b44
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-05-07 05:53:50 -0500

    Added date JS and controller

---
+++ src/main/java/org/rg25/controllers/NewDate.java
@@ -0,0 +1,49 @@
+package org.rg25.controllers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.rg25.entity.Date;
+import org.rg25.entity.User;
+import org.rg25.persistance.GenericDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+@WebServlet(
+        urlPatterns = {"/newDate"}
+)
+public class NewDate extends HttpServlet {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    private GenericDao<Date> dateDao = new GenericDao<>(Date.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("user");
+
+        // No user, default to error page
+        if (session.getAttribute("user") == null) {
+            RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/error.jsp");
+            dispatch.forward(req, resp);
+            return;
+        }
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        java.util.Date currentDate = new java.util.Date();
+        Date date = new Date(user, "New Date", "", format.format(currentDate));
+
+        int id = dateDao.insert(date);
+
+        session.setAttribute("title", "Edit Date");
+
+        resp.sendRedirect("dateEditor?id=" + id);
+    }
+
+}
