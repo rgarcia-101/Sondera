f13797fa793d77efa873a97556aae2d8172c8e5d
ruben-g922 <143372519+ruben-g922@users.noreply.github.com>
2024-05-06 19:42:10 -0500

    Todo controllers

---
+++ src/main/java/org/rg25/controllers/TodoController.java
@@ -0,0 +1,115 @@
+package org.rg25.controllers;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONException;
+import org.rg25.entity.Bookmark;
+import org.rg25.entity.Note;
+import org.rg25.entity.Todo;
+import org.rg25.entity.User;
+import org.rg25.persistance.GenericDao;
+import org.rg25.util.ServletUtil;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+@WebServlet(
+        urlPatterns = {"/todoEditor"}
+)
+public class TodoController extends HttpServlet {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    private GenericDao<Todo> todoDao = new GenericDao<>(Todo.class);
+    private ServletUtil util = new ServletUtil();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String todoId = req.getParameter("id");
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("user");
+        if (todoId == null || !todoId.matches("\\d+") || user == null) {
+            RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/error.jsp");
+            dispatch.forward(req, resp);
+            return;
+        }
+        Todo todo = todoDao.getById(Integer.parseInt(todoId));
+        if (!user.equals(todo.getUser())) {
+            logger.debug("User does not match!");
+            logger.debug(user);
+            logger.debug(todo.getUser());
+            RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/error.jsp");
+            dispatch.forward(req, resp);
+            return;
+        }
+
+        String url = "/todoEditor.jsp";
+        req.getSession().setAttribute("object", todo);
+        req.getSession().setAttribute("callback", "todos");
+        RequestDispatcher dispatch = getServletContext().getRequestDispatcher(url);
+        dispatch.forward(req, resp);
+
+    }
+
+
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        StringBuilder buffer;
+        String line = null;
+        User user = (User) req.getSession().getAttribute("user");
+        if (user == null) {
+            logger.error("No user detected!");
+            throw new ServletException();
+        }
+
+        buffer = util.readRequest(req);
+
+        resp.setContentType("application/json");
+        PrintWriter writer = resp.getWriter();
+        try {
+            JsonObject content = JsonParser.parseString(buffer.toString()).getAsJsonObject();
+            logger.info("incoming json: " + content.toString());
+
+            String description = content.get("textContent").getAsString();
+            int id = content.get("id").getAsInt();
+            String title = content.get("todoTitle").getAsString();
+            String dueDate = content.get("dueDate").getAsString();
+            dueDate += " 00:00:00";
+            Todo todo = todoDao.getById(id);
+            if (todo.getContent().equals(description) && todo.getTitle().equals(title) &&
+                    todo.getDueDate().equals(dueDate)) {
+                util.approveEquivalentJson(writer);
+                return;
+            }
+
+
+
+            if (user.getId() == todo.getUser().getId()) {
+                logger.info("Attempting to update..." + todo);
+                todo.setTitle(title);
+                todo.setContent(description);
+                todo.setDueDate(dueDate);
+                todoDao.update(todo);
+                JsonObject jsonResponse = new JsonObject();
+                jsonResponse.addProperty("responseCode", "0");
+                writer.print(jsonResponse.toString());
+                writer.flush();
+            }
+        } catch (JSONException e) {
+            util.denyJson(writer);
+            logger.error("Could not parse JSON! " + e);
+        } catch (Exception e) {
+            util.denyJson(writer);
+            logger.error("Something went wrong saving a note! " + e);
+        }
+    }
+}
--- src/main/java/org/rg25/controllers/TodoHomeController.java
+++ src/main/java/org/rg25/controllers/TodoHomeController.java
@@ -15,12 +15,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @WebServlet(
         urlPatterns = {"/todos"}
 )
 public class TodoHomeController extends HttpServlet {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
@@ -33,12 +40,59 @@ public class TodoHomeController extends HttpServlet {
             return;
         }
 
-        List<Todo> todoList = user.getTodos();
+        List<Todo> todos = user.getTodos();
+        List<List<Todo>> todoList = null;
+        try {
+            todoList = processDates(todos);
+        } catch (Exception e) {
+            logger.error("Could not process dates! " + e);
+        }
+
 
         String url = "/todosHome.jsp";
         req.setAttribute("todos", todoList);
         RequestDispatcher dispatch = getServletContext().getRequestDispatcher(url);
         dispatch.forward(req, resp);
+    }
+
+    private List<List<Todo>> processDates(List<Todo> todos) throws Exception {
+        List<Todo> overdue = new ArrayList<>();
+        List<Todo> today = new ArrayList<>();
+        List<Todo> soon = new ArrayList<>();
+        List<Todo> later = new ArrayList<>();
+
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date now = new Date();
+        Date tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        Date week = new Date();
+        week.setDate(week.getDate() + 7);
+        Date date;
+        for (Todo todo : todos) {
+            date = format.parse(todo.getDueDate());
+            if (date.before(now)) {
+                logger.info("overdue");
+                overdue.add(todo);
+            } else if (date.before(tomorrow)) {
+                logger.info("tomorrow");
+                today.add(todo);
+            } else if (date.before(week)) {
+                logger.info("week");
+                soon.add(todo);
+            } else {
+                logger.info("month");
+                later.add(todo);
+            }
+        }
+        List<List<Todo>> sortedTodo = new ArrayList<>();
+        sortedTodo.add(overdue);
+        sortedTodo.add(today);
+        sortedTodo.add(soon);
+        sortedTodo.add(later);
+
+        logger.info(sortedTodo);
+        return sortedTodo;
+
     }
 
     }
